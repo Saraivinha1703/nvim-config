7a0ec8fe01bab1f303722d7b856ef5895c3ab129
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2025-11-14 09:47:06 +0000

    Using VS Code theme because I'm used to these colors on C# (sorry nvim people)

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,6 +5,28 @@ return {
 		config = function()
 			vim.cmd.colorscheme("catppuccin")
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true })
+			vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true })
+			vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { undercurl = true })
+		end,
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark"
+			local c = require("vscode.colors").get_colors()
+			require("vscode").setup({
+				transparent = true,
+				italic_comments = true,
+				group_overrides = {
+					["@lsp.type.interface"] = { fg = c.vscLightGreen },
+					["@lsp.type.typeParameter"] = { fg = c.vscLightGreen },
+					["@lsp.type.namespace"] = { fg = c.vscFront },
+					["@lsp.type.property"] = { fg = c.vscFront },
+					["@lsp.type.field.cs"] = { fg = c.vscFront },
+				},
+			})
 		end,
 	},
 	{
