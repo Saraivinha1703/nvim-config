efd70988696208d51371557bfc4a0484aad14185
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2025-11-14 09:45:11 +0000

    Adding gitsigns, statuscol and ibl plugins

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -8,6 +8,13 @@ return {
 		"tpope/vim-fugitive",
 	},
 	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+			vim.cmd "set statusline+=%{get(b:,'gitsigns_status','')}"
+		end,
+	},
+	{
 		"brenoprata10/nvim-highlight-colors",
 		config = function()
 			require("nvim-highlight-colors").setup({})
---
+++ lua/plugins/statuscol.lua
@@ -0,0 +1,28 @@
+return {
+	"luukvbaal/statuscol.nvim",
+	config = function()
+		local builtin = require("statuscol.builtin")
+		require("statuscol").setup({
+			-- configuration goes here, for example:
+			relculright = true,
+			segments = {
+				{
+					text = { builtin.foldfunc },
+					click = "v:lua.ScFa",
+				},
+				{
+					sign = { namespace = { "gitsigns" }, maxwidth = 2, auto = true },
+					click = "v:lua.ScSa",
+				},
+				{
+					text = { builtin.lnumfunc, "  " },
+					click = "v:lua.ScLa"
+				},
+				{
+					sign = { name = { ".*" }, maxwidth = 2, colwidth = 2, auto = true, wrap = true },
+					click = "v:lua.ScSa",
+				},
+			},
+		})
+	end,
+}
